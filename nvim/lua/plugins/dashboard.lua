71ed9c1f8f4842ac874d252ecb5de9c903a0f093
Bongo Poyo <hashir447079@gmail.com>
2025-07-23 06:59:39 +0500

    Lazygit integration

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -2,6 +2,7 @@ return {
   "folke/snacks.nvim",
   opts = {
     dashboard = {
+
       preset = {
         header = table.concat({
           "██████   ██████  ███    ██  ██████   ██████ ",
@@ -17,11 +18,7 @@ return {
           "███   ████   ████   ██ ██      ██",
         }, "\n"),
 
-        footer = {function()
-          local stats = require("lazy.stats")
-          return string.format("⚡ BongoNvim loaded %d/%d plugins in %.2fms", stats.loaded, stats.count, stats.startuptime)
-        end,
-        }
+
       },
     },
   },
