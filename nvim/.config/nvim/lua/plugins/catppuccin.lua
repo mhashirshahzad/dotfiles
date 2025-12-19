a055597d811f9967a0e8a7a243a36ebdf2b9a567
Bongo <bongopoyo@proton.me>
2025-12-19 14:42:31 +0500

    replaced pink and blue :>

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -4,12 +4,24 @@ return {
     name = "catppuccin",
     opts = {
       dim_inactive = { enabled = true, percentage = 0.25 },
+
+      -- swap blue and pink
+      color_overrides = {
+        mocha = {
+          blue = "#f5c2e7",
+          pink = "#89b4fa",
+        },
+      },
+
       highlight_overrides = {
         mocha = function(c)
           return {
             Normal = { bg = c.mantle },
             Comment = { fg = "#7687a0" },
-            ["@tag.attribute"] = { style = {} },
+            TabLineSel = { bg = c.pink },
+            ["@tag.attribute"] = { fg = c.pink },
+            ["@tag"] = { fg = c.pink },
+            ["@tag.delimiter"] = { fg = c.pink },
           }
         end,
       },
