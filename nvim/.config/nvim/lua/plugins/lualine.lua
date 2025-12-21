af1fbe2cf6598d87df000966ab1028c59c33a9c9
Bongo <bongopoyo@proton.me>
2025-12-21 16:32:27 +0500

    Added custom status line

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,21 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup {
+      options = {
+        section_separators = "",
+        component_separators = "",
+        globalstatus = true, -- single bottom bar
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "diagnostics", "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    }
+  end,
+}
