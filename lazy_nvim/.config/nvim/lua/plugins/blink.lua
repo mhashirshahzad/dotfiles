c3fcec0b4aeef4b8a8fda12afb357191f53f04dd
Hashir <hashir447079@gmail.com>
2026-09-07 21:01:02 +0500

    Haya

---
+++ lazy_nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<Tab>"] = { "select_next", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "fallback" },
+      },
+    },
+  },
+}
